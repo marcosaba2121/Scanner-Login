e32a7020a3fb08fa5908d0857a830951b11948b2
Marcos Abadi <marcosaba2121@gmail.com>
2017-04-12 00:43:57 -0500

    test

---
+++ js/index.js
@@ -0,0 +1,36 @@
+var working = false;
+var email = $("#emailLogin").val();
+var password = $("#passwordLogin").val();
+
+$('.login').on('submit', function(e) {
+  e.preventDefault();
+  if (working) return;
+  working = true;
+  var $this = $(this),
+    $state = $this.find('button > .state');
+  $this.addClass('loading');
+  $state.html('Authenticating');
+  setTimeout(function() {
+    $this.addClass('ok');
+    $state.html('Welcome back!');
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      alert(errorMessage);
+    });
+    setTimeout(function() {
+      $state.html('Log in');
+      $this.removeClass('ok loading');
+      working = false;
+    }, 4000);
+  }, 3000);
+});
+
+firebase.auth().onAuthStateChanged(function(user) {
+ if (user) {
+   // User is signed in.
+   window.location = "http://scanner.ficik.com"
+
+ }
+});
